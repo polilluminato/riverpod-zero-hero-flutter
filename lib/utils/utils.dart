a3a4184707778f8d579c23f20dd7999ae8a6300b
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-09-10 17:27:59 +0200

    chore: upgrade dependencies

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-gapH(double height) => SizedBox(
-      height: height,
-    );
+gapH(double height) => SizedBox(height: height);
 
-gapW(double width) => SizedBox(
-      width: width,
-    );
+gapW(double width) => SizedBox(width: width);
 
 Future<void> goToUrl(String url) async {
   if (!await launchUrl(Uri.parse(url))) {
