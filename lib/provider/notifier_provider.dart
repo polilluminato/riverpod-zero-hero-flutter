61a2413196652ed86efbbbaad7ed24833dda853f
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-09-18 06:37:49 +0200

    fix: Notifier provider with code generation

--- lib/provider/notifier_provider.dart
+++ lib/provider/notifier_provider.dart
@@ -1,8 +1,8 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_zero_hero_thecmmbay/provider/state_class.dart';
 
-//With code generation
-/*part 'notifier_provider.g.dart';
+part 'notifier_provider.g.dart';
+
 @riverpod
 class Pizza extends _$Pizza {
   @override
@@ -24,30 +24,4 @@ class Pizza extends _$Pizza {
   void changeCondimento(String newCondimentoLink) {
     state = state.copyWith(linkCondimento: newCondimentoLink);
   }
-}*/
-
-//Without code generation
-class PizzaNotifier extends Notifier<MyPizza> {
-  @override
-  MyPizza build() {
-    return MyPizza(
-        linkImpasto: impastoOptionList[0].link,
-        linkCondimento: condimentoOptionList[0].link,
-        linkSalsa: salsaOptionList[0].link);
-  }
-
-  void changeImpasto(String newImpastoLink) {
-    state = state.copyWith(linkImpasto: newImpastoLink);
-  }
-
-  void changeSalsa(String newSalsaLink) {
-    state = state.copyWith(linkSalsa: newSalsaLink);
-  }
-
-  void changeCondimento(String newCondimentoLink) {
-    state = state.copyWith(linkCondimento: newCondimentoLink);
-  }
 }
-
-final pizzaProvider =
-    NotifierProvider<PizzaNotifier, MyPizza>(() => PizzaNotifier());
---
+++ lib/provider/notifier_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notifier_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pizzaHash() => r'980b025bb049a58b3f8907d210c16af0181c7025';
+
+/// See also [Pizza].
+@ProviderFor(Pizza)
+final pizzaProvider = AutoDisposeNotifierProvider<Pizza, MyPizza>.internal(
+  Pizza.new,
+  name: r'pizzaProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pizzaHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Pizza = AutoDisposeNotifier<MyPizza>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
